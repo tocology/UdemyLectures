cf38a528a7a7be7fbcd61cbccd2d0bb1eda476fd
tocology <hwangjun7777@gmail.com>
2015-09-07 14:48:27 +0900

    heroku setting

---
+++ AllAboutNode/chatcat/heroku_setting.md
@@ -0,0 +1,9 @@
+## heroku access at that root folder. * you need administrator.
+```
+  heroku login
+```
+## setting git
+```
+  git init
+  heroku git:remote -a chatcat
+```
--- AllAboutNode/chatcat/socket/socket.js
+++ AllAboutNode/chatcat/socket/socket.js
@@ -9,4 +9,36 @@ module.exports = function(io, rooms){
       socket.emit('roomupdate', JSON.stringify(rooms));
     });
   });
+
+  var messages = io.of('/messages').on('connection', function(socket){
+    console.log('Connected to the Chatroom !');
+
+    socket.on('joinroom', function(data){
+      socket.username = data.user;
+      socket.userPic = data.userPic;
+      socket.join(data.room);
+      updateUserList(data.room, true);
+    });
+
+    socket.on('newMessage', function(data){
+      socket.broadcast.to(data.room_number).emit('messageFeed', JSON.stringify(data));
+    });
+
+    function updateUserList(room, updateAll){
+      var getUsers = io.of('/messages').clients(room);
+      var userlist = [];
+      for(var i in getUsers){
+        userlist.push({user:getUsers[i].username, userPic:getUsers[i].userPic});
+      }
+      socket.to(room).emit('updateUserList', JSON.stringify(userlist));
+
+      if(updateAll){
+        socket.broadcast.to(room).emit('updateUserList', JSON.stringify(userlist));
+      }
+    }
+
+    socket.on('updateList', function(data){
+      updateUserList(data.room);
+    })
+  })
 }
